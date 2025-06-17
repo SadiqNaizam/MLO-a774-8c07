8627abc4a8adead28d222b9cb20750c12210aca9
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-17 15:59:15 +0530

    Files generated: Sidebar.tsx

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import SidebarNav from '../Dashboard/SidebarNav';
+
+interface SidebarProps {
+  className?: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ className }) => {
+  return (
+    <SidebarNav className={cn("h-full w-full", className)} />
+  );
+};
+
+export default Sidebar;
